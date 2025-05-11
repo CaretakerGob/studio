98800990fdb2e590e2bab771d35b0c727d9d160e
Gob Walker <rotbcaretaker@gmail.com>
2025-05-11 21:19:50 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <SidebarHeader className="p-4 flex items-center justify-between">
    
    with the text content:
    
        RotB Companion
    
    Relevant files:
    
        - https://6000-firebase-studio-1746941702181.cluster-t23zgfo255e32uuvburngnfnn4.cloudworkstations.dev/_next/static/chunks/src_88414961._.js
        - https://6000-firebase-studio-1746941702181.cluster-t23zgfo255e32uuvburngnfnn4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    remove icon

--- src/components/layout/app-sidebar.tsx
+++ src/components/layout/app-sidebar.tsx
@@ -30,10 +30,7 @@ export function AppSidebar() {
     <Sidebar collapsible="icon" className="border-r border-sidebar-border shadow-lg">
       <SidebarHeader className="p-4 flex items-center justify-between">
         <Link href="/" className="flex items-center gap-2 group-data-[collapsible=icon]:hidden">
-          {/* Beast Face Icon */}
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-8 h-8 text-primary">
-            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.41 0-8-3.59-8-8s3.59-8 8-8 8 3.59 8 8-3.59 8-8 8zm-2.5-9c.83 0 1.5-.67 1.5-1.5S10.33 8 9.5 8 8 8.67 8 9.5s.67 1.5 1.5 1.5zm5 0c.83 0 1.5-.67 1.5-1.5S15.33 8 14.5 8s-1.5.67-1.5 1.5.67 1.5 1.5 1.5zm-2.5 4c-1.48 0-2.75-.81-3.45-2H15.95c-.7 1.19-1.97 2-3.45 2zm-4.09-4.56c.1-.19.1-.41-.04-.59l-1.17-1.6c-.22-.3-.61-.38-.91-.18-.3.22-.38.61-.18.91l1.18 1.6c.15.2.38.29.59.29.08 0 .17-.02.23-.07zm8.18 0c.06.05.15.07.23.07.21 0 .44-.09.59-.29l1.18-1.6c.2-.3.12-.69-.18-.91-.3-.2-.69-.12-.91.18l-1.17 1.6c-.14.18-.14.41-.04.59z"/>
-          </svg>
+          {/* Beast Face Icon Removed */}
           <span className="text-xl font-semibold text-sidebar-foreground">RotB Companion</span>
         </Link>
         <SidebarTrigger asChild className="md:hidden">
@@ -69,3 +66,4 @@ export function AppSidebar() {
     </Sidebar>
   );
 }
+
